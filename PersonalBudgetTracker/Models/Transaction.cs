61dddd3a79db6b67fd1e008c750aedcca6ea05ba
jkjorgie <jkjorgie@yahoo.com>
2026-03-25 21:03:29 -0600

    merge conflict resolution complete

--- PersonalBudgetTracker/Models/Transaction.cs
+++ PersonalBudgetTracker/Models/Transaction.cs
@@ -8,15 +8,12 @@ public class Transaction
 {
     public int TransactionId { get; set; }
 
-    [Required]
     public string UserId { get; set; } = "";
     public ApplicationUser User { get; set; } = null!;
 
-    [Required]
     [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0.")]
     public decimal Amount { get; set; }
 
-    [Required]
     public TransactionType Type { get; set; }
 
     [Required]
